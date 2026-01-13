288b51234b77413c66c74e75a2c9a32e951a0efa
Sirawit <sirawit.k@kkumail.com>
2026-01-13 18:29:15 +0700

    lab05update

--- apps/lab04-photo-vue/ios/App/CapApp-SPM/Package.swift
+++ apps/lab04-photo-vue/ios/App/CapApp-SPM/Package.swift
@@ -11,14 +11,28 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.0"),
+        .package(name: "CapacitorApp", path: "..\..\..\..\..\node_modules\.pnpm\@capacitor+app@8.0.0_@capacitor+core@8.0.0\node_modules\@capacitor\app"),
+        .package(name: "CapacitorCamera", path: "..\..\..\..\..\node_modules\.pnpm\@capacitor+camera@8.0.0_@capacitor+core@8.0.0\node_modules\@capacitor\camera"),
+        .package(name: "CapacitorFilesystem", path: "..\..\..\..\..\node_modules\.pnpm\@capacitor+filesystem@8.0.0_@capacitor+core@8.0.0\node_modules\@capacitor\filesystem"),
+        .package(name: "CapacitorHaptics", path: "..\..\..\..\..\node_modules\.pnpm\@capacitor+haptics@8.0.0_@capacitor+core@8.0.0\node_modules\@capacitor\haptics"),
+        .package(name: "CapacitorKeyboard", path: "..\..\..\..\..\node_modules\.pnpm\@capacitor+keyboard@8.0.0_@capacitor+core@8.0.0\node_modules\@capacitor\keyboard"),
+        .package(name: "CapacitorPreferences", path: "..\..\..\..\..\node_modules\.pnpm\@capacitor+preferences@8.0.0_@capacitor+core@8.0.0\node_modules\@capacitor\preferences"),
+        .package(name: "CapacitorStatusBar", path: "..\..\..\..\..\node_modules\.pnpm\@capacitor+status-bar@8.0.0_@capacitor+core@8.0.0\node_modules\@capacitor\status-bar")
     ],
     targets: [
         .target(
             name: "CapApp-SPM",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "CapacitorApp", package: "CapacitorApp"),
+                .product(name: "CapacitorCamera", package: "CapacitorCamera"),
+                .product(name: "CapacitorFilesystem", package: "CapacitorFilesystem"),
+                .product(name: "CapacitorHaptics", package: "CapacitorHaptics"),
+                .product(name: "CapacitorKeyboard", package: "CapacitorKeyboard"),
+                .product(name: "CapacitorPreferences", package: "CapacitorPreferences"),
+                .product(name: "CapacitorStatusBar", package: "CapacitorStatusBar")
             ]
         )
     ]
